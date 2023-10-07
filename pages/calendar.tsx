576a6191de5e2c009b3d347ae469cb2b137f2ed3
canhamzacode <abdulmuizzayo6@gmail.com>
2023-10-07 17:48:43 +0200

    pulled from the main branch

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,9 +1,13 @@
 import MainLayout from "@/components/layout/mainLayout";
 import MyCalendar from "@/components/layout/myCalendar";
+import Header from "@/components/header";
 export default function Calendar() {
     return (
       <MainLayout>
-        <MyCalendar />
+        <div className="grid">
+          <Header title={"Calendar"} info={"Stay Connected to Your People’s Events."} />
+          <MyCalendar />
+        </div>
       </MainLayout>
     )
 }
