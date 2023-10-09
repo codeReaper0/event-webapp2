75abaef71f1cabcf03fe0e6e3ba8e9481b7a2dfb
Tella Boluwatife <42048129+codeReaper0@users.noreply.github.com>
2023-10-08 18:44:50 -0700

    Dev (#27)
    * updated overflow issues
    
    * EVE-23/feat/auth (#15)
    
    * feat: create auth page
    
    * feat: add image on mobile and make responsive oon tab view
    
    * Eve 24/feat/create event (#16)
    
    * created the create-events UI
    
    * cretaed a responsive create-event page
    
    * Eve 24/feat/create event (#18)
    
    * created the create-events UI
    
    * cretaed a responsive create-event page
    
    * fix: my-people card component (#24)
    
    * Eve 24/feat/create event (#23)
    
    * created the create-events UI
    
    * cretaed a responsive create-event page
    
    * revamped and added harmburger
    
    * added build file
    
    ---------
    
    Co-authored-by: Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
    Co-authored-by: Sultan Adeleke <70979325+sultyl@users.noreply.github.com>
    Co-authored-by: Etanami Olatunji <etanamiolatunji@gmail.com>

--- components/layout/myTimeline.tsx
+++ components/layout/myTimeline.tsx
@@ -1,25 +1,31 @@
-
 import TimelineCard from "./TimelineCard";
 
 const MyTimeline: React.FC = () => {
-
-    return (
-        <div className='w-full h-[87vh] my-8 pb-8 overflow-auto scrollbar-w-0'>
-            {/* Discover and Create */}
-            <div className="relative bg-[url('/Rectangle27.png')] bg-cover bg-center min-h-[300px]">
-                <div className="absolute inset-0 bg-gradient-to-r from-[#3F3849] via-[rgba(63, 56, 73, 0.83)] to-[rgba(63, 56, 73, 0.00)] z-0"></div>
-                <div className="p-12">
-                    <div className="max-w-[420px] w-full font-sans relative z-10">
-                        <h1 className='text-white font-bold text-[28px] leading-[42px]'>Discover and Create Memorable Events</h1>
-                        <p className="text-[#FFFFFF80] mt-1 text-base font-medium">Craft events that reflect your passions and interests.</p>
-                        <button className='mt-8 flex gap-2 items-center py-3 px-4 text-black bg-[#FFC6BC] rounded-2xl font-sans font-medium active:bg-[#fadfc8] active:scale-[0.98]'>Create An Event</button>
-                    </div>
-                </div>
-            </div>
-            {/* Timeline Card Section */}
-            <TimelineCard />
+  return (
+    <div className="w-full h-[87vh] my-8 pb-8 scrollbar-w-0">
+      {/* Discover and Create */}
+      <div className="relative bg-[url('/Rectangle27.png')] bg-cover bg-center min-h-[300px]">
+        <div className="absolute inset-0 bg-gradient-to-r from-[#3F3849] via-[rgba(63, 56, 73, 0.83)] to-[rgba(63, 56, 73, 0.00)] z-0"></div>
+        <div className="p-12">
+          <div className="max-w-[420px] w-full font-sans relative z-10">
+            <h1 className="text-white font-bold text-[28px] leading-[42px]">
+              Discover and Create Memorable Events
+            </h1>
+            <p className="text-[#FFFFFF80] mt-1 text-base font-medium">
+              Craft events that reflect your passions and interests.
+            </p>
+            <form action={'/timeline/create-events'}>
+              <button type='submit' className="mt-8 flex gap-2 items-center py-3 px-4 text-black bg-[#FFC6BC] rounded-2xl font-sans font-medium active:bg-[#fadfc8] active:scale-[0.98]">
+                Create An Event
+              </button>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+      {/* Timeline Card Section */}
+      <TimelineCard />
+    </div>
+  );
 };
 
 export default MyTimeline;
