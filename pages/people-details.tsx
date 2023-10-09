94e2fc31089d54ceb510991f4e88eabeecb4e2c0
Ebuka Ejiofor <89203154+ebukathedev@users.noreply.github.com>
2023-10-10 00:33:01 +0100

    Eve 11/feat/event card (#36)
    * create Event Card component
    
    * add EventCard component to the people-details      page UI
    
    * make the eventCard responsive

--- pages/people-details.tsx
+++ pages/people-details.tsx
@@ -1,3 +1,4 @@
+import EventCard from "@/components/eventCard";
 import MainLayout from "@/components/layout/mainLayout";
 import MyPeopleCardDetails from "@/components/myPeopleDetailsCard";
 import PeopleHeader from "@/components/peopleHeader";
@@ -6,10 +7,10 @@ export default function PeopleDetails() {
   return (
     <MainLayout>
       <PeopleHeader />
-      <div className=" w-full p-6 grid grid-cols-3 gap-6">
-        <MyPeopleCardDetails />
-        <MyPeopleCardDetails />
-        <MyPeopleCardDetails />
+      <div className=" w-full p-6 grid gap-6 justify-center min-[980px]:justify-start min-[980px]:grid-cols-2 min-[1230px]:grid-cols-3">
+        <EventCard />
+        <EventCard />
+        <EventCard />
       </div>
     </MainLayout>
   );
