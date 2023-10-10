8b6222e2ea5947344389444dbbafadb7a7b31a1d
codeReaper0 <boluwatifetella@gmail.com>
2023-10-10 02:09:30 +0100

    adjusted layout

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -7,24 +7,24 @@ import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <aside className="w-[320px] bg-primary h-full lg:flex lg:flex-col md:flex md:flex-col xsm:hidden justify-between pb-8 rounded-r-3xl">
+    <aside className="w-[320px] bg-primary h-full hidden lg:flex lg:flex-col justify-between pb-8 rounded-r-3xl">
       <div className="w-full">
         {/* Icon */}
         <div className="inline-flex items-center gap-2 mt-6 mx-14 mb-16">
-                <Image
-                  src={Logo}
-                  alt="logo"
-                  width={201}
-                  height={54}
-                  className="w-auto h-auto"
-                />
-                <h3 className="text-brand-gray-100 text-lg font-bold">WetinDeySup</h3>
-              </div>
+          <Image
+            src={Logo}
+            alt="logo"
+            width={201}
+            height={54}
+            className="w-auto h-auto"
+          />
+          <h3 className="text-brand-gray-100 text-lg font-bold">WetinDeySup</h3>
+        </div>
 
         <NavLinks />
       </div>
 
-      <Link href='/'>
+      <Link href="/">
         <button className="flex gap-3 items-center text-white/50 font-medium px-12">
           <LogoutIcon />
           <span>Log Out</span>
