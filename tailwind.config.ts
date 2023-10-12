76c4111297ed08dc0cb2cd383110a886593bdeae
codeReaper <boluwatifetella@gmail.com>
2023-10-12 23:20:06 +0100

    finish adjusting the config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
         montserrat: "var(--font-montserrat)",
       },
       screens: {
-        xsm: "360px", // Adjust the breakpoint as needed
+        xsm: "360px",
       },
       colors: {
         primary: "#3F3849",
