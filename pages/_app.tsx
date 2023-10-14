27d26cbbf1b4da5ed794f89ddcab35fa51973d4e
Tella Boluwatife <42048129+codeReaper0@users.noreply.github.com>
2023-10-13 21:39:26 -0700

    Dev (#70)
    * created the create-events UI
    
    * cretaed a responsive create-event page
    
    * revamped and added harmburger
    
    * added build file
    
    * made correction
    
    * made calendar functional
    
    * added form validation and sending dtata to endpoint
    
    * added form validation and sending dtata to endpoint
    
    * added toastify to show sucess message and error message
    
    * Timeline Event card data fetched
    
    * dynamic search bar
    
    * fix
    
    * worked
    
    * removed all console
    
    * removed search bar
    
    * cleanup
    
    * final push
    
    * adjiusted sidebar spacing
    
    * fixed timeline dropdown
    
    * description error
    
    * setting up routes (#67)
    
    * EVE2-10/feat/auth (#72)
    
    * feat: add auth with google
    
    * fix: display error message with toast
    
    * feat: handle protected routing
    
    * Eve2 07/feat/seach component and search results page (#71)
    
    * made a searchbar component and adjusted the header classes
    
    * Added search results page
    
    * added api call
    
    ---------
    
    Co-authored-by: Tella Boluwatife <42048129+codeReaper0@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sultyl <70979325+sultyl@users.noreply.github.com>
    Co-authored-by: canhamzacode <abdulmuizzayo6@gmail.com>
    Co-authored-by: AJAYI Toheeb Opeyemi <toheebopeyemi9@gmail.com>
    Co-authored-by: Oluwatosin David Oduwole <108693106+david2tosin@users.noreply.github.com>
    Co-authored-by: Judge-Paul Ogebe <110723341+Judge-Paul@users.noreply.github.com>
    Co-authored-by: Nosa10 <101909678+Nosa10@users.noreply.github.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import type { Metadata } from "next";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import http from "@/http/interceptor";
 
 export const metadata: Metadata = {
   title: "Events App",
@@ -8,5 +11,28 @@ export const metadata: Metadata = {
 };
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = async (url: string) => {
+      const response = await http.get("/profile/");
+      console.log(response);
+      const { message } = response.data;
+      if (message !== "User login successful" && (url !== "/" || "/auth")) {
+        router.replace("/auth");
+      } else if (
+        message === "User login successful" &&
+        (url === "/" || "/auth")
+      ) {
+        router.replace("/timeline");
+      }
+    };
+
+    router.events.on("routeChangeStart", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChange);
+    };
+  }, []);
   return <Component {...pageProps} />;
 }
